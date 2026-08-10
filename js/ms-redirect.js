aa3a84603c9a5359e7be8ae075460e2d167da0ef
Zanderson73 <your-github-isc.anderson.z@gmail.com>
2026-08-10 13:52:26 +0200

    Complete rebuild: syncing clean local files

---
+++ js/ms-redirect.js
@@ -0,0 +1,6 @@
+// ms-redirect.js
+// Currently disabled – MemberSpace handles protection via page locks + ?msopen= links
+(function() {
+  'use strict';
+  console.log("ms-redirect.js loaded – no forced redirects active.");
+})();
